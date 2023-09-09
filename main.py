3fe45219378a0db8977f78b3a9857b8355555f9e
SashaKuspys <134163412+SashaKuspys@users.noreply.github.com>
2023-09-09 13:58:19 +0300

    Sort_folder
    підкоригував файл main.py, щоб він викликав sort_folder

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
+# import address_book
+# import notes_book
+import sort_folder
+from pathlib import Path
+
+
 while True:
     print("Меню:")
     print("1. Вибір 1")
     print("2. Вибір 2")
     print("3. Вибір 3")
     print("0. Вихід")
+
     choice = input("Введіть номер вибору: ")
+    
     if choice == "1":
-        # Викликати функцію або виконати дії для вибору 1
+        # address_book.main()
         print("Ви обрали варіант 1")
     elif choice == "2":
-        # Викликати функцію або виконати дії для вибору 2
+        # notes_book.main()
         print("Ви обрали варіант 2")
     elif choice == "3":
-        # Викликати функцію або виконати дії для вибору 3
+        folder_path = input("Введіть шлях до теки для сортування: ")
+        sort_folder.main(Path(folder_path))
         print("Ви обрали варіант 3")
     elif choice == "0":
         print("До побачення!")
